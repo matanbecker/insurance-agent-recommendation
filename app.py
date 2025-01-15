64e7fe0b1bc5394166ad7ceeb698e6166ede2349
matanbecker <matan.becker76@gmail.com>
2025-01-15 15:59:08 -0500

    Initial commit of insurance recommendation system

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ import joblib
 import openai
 from flask import Flask, request, jsonify
 
-# Set your OpenAI API key
-openai.api_key = "xxxxxx"
+
 
 # Load data
 df = pd.read_csv('synthetic_renewal_data.csv')
